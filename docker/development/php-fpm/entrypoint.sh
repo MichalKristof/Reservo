7c867b2ff35ffbfb4fd9e8dd7b46a5aaf423d653
Kristof <kristof@leadmedia.sk>
2025-06-18 17:04:54 +0200

    update entrypoint for development to install dependencies

--- docker/development/php-fpm/entrypoint.sh
+++ docker/development/php-fpm/entrypoint.sh
@@ -9,6 +9,24 @@ GROUP_ID=${GID:-1000}
 echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
 chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
 
+# Composer dependencies
+if [ ! -d /var/www/vendor ]; then
+  echo "Installing Composer dependencies…"
+  composer install --no-dev --optimize-autoloader --no-interaction
+fi
+
+# NPM dependencies
+if [ -f /var/www/package.json ] && [ ! -d /var/www/node_modules ]; then
+  echo "Installing NPM dependencies…"
+  npm install
+fi
+
+# Generate app key if missing
+if ! grep -q "^APP_KEY=" /var/www/.env || [ -z "$(grep "^APP_KEY=" /var/www/.env | cut -d'=' -f2)" ]; then
+  echo "Generating APP_KEY…"
+  php artisan key:generate --ansi --force
+fi
+
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
 php artisan config:clear
